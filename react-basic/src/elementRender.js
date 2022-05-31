0f0a8bfb831895f5792c1b32ddc1daa36619a417
feynman <2901954062@qq.com>
2022-05-31 19:09:31 +0800

    feat: basic元素渲染

--- react-basic/src/elementRender.js
+++ react-basic/src/elementRender.js
@@ -2,6 +2,21 @@
  * @Author: fg
  * @Date: 2022-05-26 19:56:46
  * @LastEditors: fg
- * @LastEditTime: 2022-05-26 19:56:47
+ * @LastEditTime: 2022-05-31 18:55:58
  * @Description: 元素渲染
  */
+import ReactDOM from 'react-dom/client';
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(element, document.getElementById('root'));
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
