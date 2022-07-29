cb4c83295a98a748d79d7573522b70c24ea78e76
Feynman <2901954062@qq.com>
2022-07-29 17:04:08 +0800

    feat: hook add init

---
+++ learn-react-hooks/src/lHooks/index.tsx
@@ -0,0 +1,10 @@
+/*
+ * @Author: fg
+ * @Date: 2022-07-29 15:45:07
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-29 16:08:51
+ * @Description: react hooks
+ */
+import {useState} from 'react';
+
+const [val, toggle] = useState(false)
