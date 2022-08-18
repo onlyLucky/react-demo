4f1f9b4c03f4191384935ae8be92fcc7a9710d1f
Feynman <2901954062@qq.com>
2022-08-18 18:09:25 +0800

    feat: useEffect init add

---
+++ learn-react-hooks/src/iHooks/useEffect.tsx
@@ -0,0 +1,8 @@
+/*
+ * @Author: fg
+ * @Date: 2022-08-18 17:33:02
+ * @LastEditors: fg
+ * @LastEditTime: 2022-08-18 18:00:19
+ * @Description: useEffect
+ */
+import React, {useState,useEffect} from "react"
