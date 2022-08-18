2a5d547c2369b86e000aedb99d06ff8946c36851
Feynman <2901954062@qq.com>
2022-08-18 17:31:19 +0800

    feat: useState hook upload

--- learn-react-hooks/src/iHooks/useState.tsx
+++ learn-react-hooks/src/iHooks/useState.tsx
@@ -2,19 +2,26 @@
  * @Author: fg
  * @Date: 2022-08-18 10:44:37
  * @LastEditors: fg
- * @LastEditTime: 2022-08-18 13:36:26
+ * @LastEditTime: 2022-08-18 17:30:41
  * @Description: useState
  */
 import React, { useState } from "react";
 
 // 给定初始化值情况下可以直接使用
+type AppProps = { message: string };
+
 const UseStateEg = () => {
   const [count, setCount] = useState(0);
-
+  const [data] = useState<AppProps | null>({
+    message: "123",
+  });
   return (
     <div>
       <p>you clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}> click me </button>
+      <br />
+      {data && data.message}
+      <div>{data?.message}</div>
     </div>
   );
 };
