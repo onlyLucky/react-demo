6431ad03b774ce86616f90ca446f5821f48f6798
Feynman <2901954062@qq.com>
2022-08-19 15:29:28 +0800

    feat: useEffect hook

--- learn-react-hooks/src/iHooks/index.tsx
+++ learn-react-hooks/src/iHooks/index.tsx
@@ -2,18 +2,20 @@
  * @Author: fg
  * @Date: 2022-07-29 15:45:07
  * @LastEditors: fg
- * @LastEditTime: 2022-08-18 13:36:44
+ * @LastEditTime: 2022-08-19 15:11:31
  * @Description: react hooks
  */
 import React from "react";
 import { Button } from "antd";
 import UseStateEg from "./useState";
+import { UseEffectEg } from "./useEffect";
 const ReactHooks = () => {
   return (
     <div>
       {/* <h1>Hello</h1>
       <Button>点我</Button> */}
       <UseStateEg></UseStateEg>
+      <UseEffectEg></UseEffectEg>
     </div>
   );
 };
--- learn-react-hooks/src/iHooks/useEffect.tsx
+++ learn-react-hooks/src/iHooks/useEffect.tsx
@@ -2,7 +2,50 @@
  * @Author: fg
  * @Date: 2022-08-18 17:33:02
  * @LastEditors: fg
- * @LastEditTime: 2022-08-18 18:00:19
+ * @LastEditTime: 2022-08-19 15:28:43
  * @Description: useEffect
  */
-import React, {useState,useEffect} from "react"
+import React, { useState, useEffect } from "react";
+
+function UseEffectEg() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `you clicked ${count} times`;
+  }, [count]); // 仅在 count 更改时更新
+
+  return (
+    <div>
+      <p>count now is {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+
+function DelayedEffect(props: { timerMs: number }) {
+  const { timerMs } = props;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // todo:
+    }, timerMs);
+    // 可选
+    return () => clearTimeout(timer);
+  }, [timerMs]);
+}
+
+//
+function UseEffectAsync() {
+  useEffect(() => {
+    (async () => {
+      const { data } = await ajax(params);
+    })();
+  }, [params]);
+  // 或者 then 也是可以的
+  useEffect(() => {
+    ajax(params).then(({ data }) => {
+      // todo
+    });
+  }, [params]);
+}
+
+export { UseEffectEg };
